236ddcbdd1cae9ce043348a45d1b3231963e3e86
Amantur-344 <amanamanovica@gmail.com>
2021-09-09 22:27:55 +0600

    change validators

--- questions/validators.py
+++ questions/validators.py
@@ -20,10 +20,9 @@ class QuestionUrlValidator:
 
     @classmethod
     def question_answer(cls, pk, account):
-        user_question = UserQuestion.objects.filter(account=account).order_by('-id').first()
-        last_user_question = Question.objects.filter(id=user_question.get_id()).first() #the last question answered by the user
-        print(last_user_question.get_id())
-        # if last_user_question == None and last_user_question.get_id() == pk - 1:
-        #     return True
-        # else:
-        #     return False
+        last_user_question = UserQuestion.objects.filter(account=account).order_by('-id').first() #the last question answered by the user
+
+        if last_user_question == None and last_user_question.question.get_id() == pk - 1:
+            return True
+        else:
+            return False

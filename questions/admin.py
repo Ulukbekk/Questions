1dbdfbd4930c94107d2e43c07596d904e91cfc87
Ulukbekk <ulukbekmomunkulov@gmail.com>
2021-09-09 16:33:19 +0600

    update admin Answer

--- questions/admin.py
+++ questions/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from questions.models import Question, Answer
 
-admin.site.register(Answer)
+@admin.register(Answer)
+class AnserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'question',
+        'answer',
+        'is_correct',
+    )
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
